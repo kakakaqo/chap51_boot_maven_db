1a00717e39d8a1323685ea6a31bcc90f5ca50478
kakakaqo <alsdn481@naver.com>
2023-06-02 17:42:16 +0900

    첫번째

---
+++ src/main/java/com/javalab/board/Chap50BootMavenDbApplication.java
@@ -0,0 +1,13 @@
+package com.javalab.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap50BootMavenDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap50BootMavenDbApplication.class, args);
+	}
+
+}
